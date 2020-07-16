b9778bb53877d3ce2fe723fca750b5e3d53af2e6
Daniele <daniele.muscia@outlook.com>
2020-07-16 16:49:53 +0200

    cookie fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className='footer'>
-            <a href='#'>Privacy Policy</a>
+            <a href='https://www.privacypolicygenerator.info/live.php?token=JxGrzClR8mdsCQJxEgtIboff86Au7eSK'>Privacy Policy</a>
             <p>Handmade with <span role='img' aria-label='Love'>❤️</span> and <span role='img' aria-label='Sofa'>🛋</span></p>
             <p>Contact us at info@netflixcodes.io</p>
         </footer>
